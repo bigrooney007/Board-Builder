8b3dec969ac19cd2dd5d736e37e09e07a20e836d
bigrooney007 <globalbusnonprofitsol@gmail.com>
2026-09-22 19:01:19 +0100

    Use clean eight-video registry

--- frontend/src/hooks/useFlowVideos.js
+++ frontend/src/hooks/useFlowVideos.js
@@ -1,14 +1,1 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
-const API = `${process.env.REACT_APP_BACKEND_URL}/api`;
-
-export const useFlowVideo = (key) => {
-  const [video, setVideo] = useState(null);
-  useEffect(() => {
-    axios.get(`${API}/flow-videos`)
-      .then((r) => setVideo((r.data.videos || []).find((v) => v.key === key) || null))
-      .catch(() => {});
-  }, [key]);
-  return video;
-};
+export { usePlatformVideo as useFlowVideo } from "@/clean/platform";
